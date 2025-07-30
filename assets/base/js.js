9dde237c98e79fff6418a01e825be5fb59b18678
roptivesolutions <roptivesolutions@gmail.com>
2025-07-30 20:57:17 +0530

    Create js.js

---
+++ assets/base/js.js
@@ -0,0 +1,31 @@
+ document.addEventListener('DOMContentLoaded', function () {
+    // Initialize chat
+    window.n8nChat.createChat({
+      webhookUrl: 'https://n8n-ipq3.onrender.com/webhook/f686bd4d-17ea-4a0b-be46-a868dd1e1ecd/chat',
+      defaultLanguage: 'en',
+      initialMessages: [
+        'Hi there!',
+        'Welcome! I’m John, your assistant. How can I support you today?'
+      ],
+      i18n: {
+        en: {
+          title: 'Roptive Properties',
+          subtitle: "🔔 We're always online.",
+          footer: '',
+          getStarted: 'New Conversation',
+          inputPlaceholder: 'Type your question..',
+        },
+      },
+    });
+
+    // Replace powered by text
+    const poweredByDiv = document.querySelector('.chat-powered-by');
+    if (poweredByDiv) {
+      const newAnchor = document.createElement('a');
+      newAnchor.href = 'https://roptive.com';
+      newAnchor.textContent = 'Roptive Solution';
+
+      poweredByDiv.innerHTML = 'Powered by ';
+      poweredByDiv.appendChild(newAnchor);
+    }
+  });
